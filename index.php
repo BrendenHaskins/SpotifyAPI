bf224fd066f37513e7befdc4086d9a91b25c8ff0
BrendenHaskins <brendenshaskins@gmail.com>
2024-04-29 11:53:06 -0400

    basic request structure

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+//inner dependencies
+require 'php/request.php';
+
 //error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
@@ -10,9 +13,13 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //define default route
-$f3->route('GET /', function(){
-    $view = new Template();
-    echo $view->render('views/home.html');
+$f3->route('GET|POST /', function(){
+    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    } else if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        echo getToken();
+    }
 });
 //run fat free
 $f3->run();
