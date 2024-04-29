c199a5ba43000bd0e63ffb688795ebb6fe2a6693
BrendenHaskins <brendenshaskins@gmail.com>
2024-04-29 12:10:09 -0400

    houston we have a problem

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-//inner dependencies
-require 'php/request.php';
-
 //error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 //requirements
 require_once('vendor/autoload.php');
+require('php/request.php');
 
 //instantiate base F3 base class
 $f3 = Base::instance();
@@ -17,8 +15,9 @@ $f3->route('GET|POST /', function(){
     if($_SERVER['REQUEST_METHOD'] == 'GET') {
         $view = new Template();
         echo $view->render('views/home.html');
-    } else if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        echo getToken();
+    } else {
+        $view = new Template();
+        echo 'NOT GET received...'.$view->render('views/home.html').getToken();
     }
 });
 //run fat free
