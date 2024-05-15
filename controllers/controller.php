5521f533c4acc811e323eebbbe5b2d634f146f23
BrendenHaskins <brendenshaskins@gmail.com>
2024-05-14 22:38:07 -0400

    controller functioning, previous guesses shown

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,8 +1,75 @@
 <?php
-//controller - define functions for routes to use.      Brenden Haskins
+
+
+//Controller class, handles methods called from the router.
+
+class Controller {
+    private $_f3; // f3 router
+
+    function __construct($f3) {
+        $this->_f3 = $f3;
+    }
 
 
 
-function testHandler() {
-    echo "This is a successful test of the controller";
+    function homePage() : void {
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $view = new Template();
+            echo $view->render('views/app.html');
+        }
+    }
+
+    function game() : void {
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            //Present the guess screen, print all former guesses.
+            $this->_f3->set('SESSION.guessCount',0);
+            $this->_f3->clear('SESSION.allGuesses');
+
+            //$this->_f3->set('allGuesses',array());
+
+
+            $view = new Template();
+            getToken($this->_f3);
+            getAllArtistsFromSetUrl($this->_f3);
+            selectHiddenArtist($this->_f3);
+            getHiddenArtistInfo($this->_f3);
+
+
+            echo $view->render('views/home.html');
+        } else if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //A guess has been submitted.
+            $oldCount = $this->_f3->get('SESSION.guessCount');
+            $this->_f3->set('SESSION.guessCount', $oldCount+1);
+
+            $userGuess = $_POST['guess'];
+
+            if($oldCount == 0) {
+                $this->_f3->set('SESSION.allGuesses', array($userGuess));
+            } else {
+                $oldGuessesArr = $this->_f3->get('SESSION.allGuesses');
+                $oldGuessesArr[] = $userGuess;
+                $this->_f3->set('SESSION.allGuesses', $oldGuessesArr);
+            }
+
+            //$this->_f3->set('SESSION.allGuesses',$this->_f3->get('SESSION.allGuesses').$userGuess);
+
+            $hiddenArtist = $this->_f3->get('SESSION.hiddenArtist');
+
+            if($userGuess == $hiddenArtist) {
+                $this->_f3->reroute('victory');
+            } else {
+                $view = new Template();
+                echo $view->render('views/home.html');
+            }
+        }
+    }
+
+    function victory() : void {
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            //User got it right.
+
+            $view = new Template();
+            echo $view->render('views/victory.html');
+        }
+    }
 }
--- index.php
+++ index.php
@@ -12,66 +12,20 @@ require_once('vendor/autoload.php');
 
 //instantiate base F3 base class
 $f3 = Base::instance();
-$controller = new
+$con = new Controller($f3);
 
 //define default route
 $f3->route('GET|POST /', function() {
-    if($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $view = new Template();
-        echo $view->render('views/app.html');
-    }
+    $GLOBALS['con']->homePage();
 });
 
 //route to play a game
 $f3->route('GET|POST /game', function($f3){
-    if($_SERVER['REQUEST_METHOD'] == 'GET') {
-        //Present the guess screen, print all former guesses.
-        $f3->set('SESSION.guessCount',0);
-
-        $f3->set('allGuesses',array());
-
-
-        $view = new Template();
-        getToken($f3);
-        getAllArtistsFromSetUrl($f3);
-        selectHiddenArtist($f3);
-
-        $printArtist = $f3->get('SESSION.hiddenArtist');
-        getHiddenArtistInfo($f3);
-
-
-        echo $view->render('views/home.html');
-    }
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //A guess has been submitted.
-        $oldCount = $f3->get('SESSION.guessCount');
-        $f3->set('SESSION.guessCount', $oldCount+1);
-
-        $userGuess = $_POST['guess'];
-
-        $f3->set('allGuesses',$f3->get('allGuesses').$userGuess);
-
-        $hiddenArtist = $f3->get('SESSION.hiddenArtist');
-
-        if($userGuess == $hiddenArtist) {
-            $f3->reroute('victory');
-        } else {
-            $view = new Template();
-            echo $view->render('views/home.html');
-        }
-    }
+    $GLOBALS['con']->game();
 });
 
 $f3->route('GET|POST /victory', function($f3) {
-    if($_SERVER['REQUEST_METHOD'] == 'GET') {
-        //User got it right.
-
-        $view = new Template();
-        echo $view->render('views/victory.html');
-    } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //TESTING
-
-    }
+    $GLOBALS['con']->victory();
 });
 
 //run fat free
